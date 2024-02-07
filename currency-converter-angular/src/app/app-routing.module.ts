564f01a4e18bb7a9d2bb08ece7db75e869689812
Fetiha Kedir <fetiha191.kedir@gmail.com>
2024-02-07 20:37:05 +0300

    header and footer added

--- currency-converter-angular/src/app/app-routing.module.ts
+++ currency-converter-angular/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { 
+    path: 'home',
+    loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule)
+  },
+{
+  path: 'details',
+    loadChildren: () => import('./features/detail/detail.module').then(m => m.DetailModule)
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- currency-converter-angular/src/app/shared/shared.module.ts
+++ currency-converter-angular/src/app/shared/shared.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { CurrencyConverterComponent } from './components/currency-converter/currency-converter.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    CurrencyConverterComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule,FormsModule, ReactiveFormsModule
+  ],
+  exports: [HeaderComponent, FooterComponent]
 })
 export class SharedModule { }
